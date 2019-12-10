b5234a34b1da347dcfbeb5c21162fc93c78dab88
axetroy <axetroy.dev@gmail.com>
2019-12-10 17:22:00 +0800

    bump to v0.1.2

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 	app.Name = "dvm"
 	app.Usage = "version management for Deno"
-	app.Version = "0.1.1"
+	app.Version = "0.1.2"
 	app.Authors = []*cli.Author{
 		{
 			Name:  "Axetroy",
