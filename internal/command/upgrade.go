1aea7473abccaa9158aa5c3f88aa9075f854a967
axetroy <axetroy.dev@gmail.com>
2019-12-12 11:31:14 +0800

    upgrade: fix not work in Windows.

--- internal/command/upgrade.go
+++ internal/command/upgrade.go
@@ -13,6 +13,7 @@ import (
 	"os/signal"
 	"path"
 	"runtime"
+	"time"
 
 	"github.com/axetroy/dvm/internal/core"
 	"github.com/axetroy/dvm/internal/util"
@@ -58,6 +59,51 @@ func getLatestVersion() (*string, error) {
 	version := response.TagName
 
 	return &version, nil
+}
+
+func upgradeCommand(newFilepath, oldFilepath string) (err error) {
+	var (
+		old os.FileInfo
+	)
+
+	old, err = os.Stat(oldFilepath)
+
+	if err != nil {
+		return err
+	}
+
+	// cover the binary file
+	if runtime.GOOS == "windows" {
+		oldFilepathBackup := path.Join(core.CacheDir, old.Name()) + fmt.Sprintf(".%d.old", time.Now().UnixNano())
+		if err = os.Rename(oldFilepath, oldFilepathBackup); err != nil {
+			return errors.Wrapf(err, "backup old version fail")
+		}
+
+		defer func() {
+			// if upgrade fail
+			if err != nil {
+				// try rollback
+				if err = os.Rename(oldFilepathBackup, oldFilepath); err != nil {
+					err = errors.Wrap(err, "rollback fail")
+					return
+				}
+			} else {
+				_ = os.Remove(oldFilepathBackup)
+				return
+			}
+		}()
+
+		// rename downloaded dvm to exist dvm filepath
+		if err = os.Rename(newFilepath, oldFilepath); err != nil {
+			return errors.Wrapf(err, "rename downloaded file to dvm filepath fail")
+		}
+	} else {
+		if err = os.Rename(newFilepath, oldFilepath); err != nil {
+			err = errors.Wrapf(err, "rename `%s` to `%s` fail", newFilepath, oldFilepath)
+		}
+	}
+
+	return
 }
 
 func Upgrade(version string, force bool) error {
@@ -131,9 +177,8 @@ func Upgrade(version string, force bool) error {
 		downloadedDvmFilepath += ".exe"
 	}
 
-	// cover the binary file
-	if err := os.Rename(downloadedDvmFilepath, dvmExecutablePath); err != nil {
-		err = errors.Wrapf(err, "rename `%s` to `%s` fail", downloadedDvmFilepath, dvmExecutablePath)
+	if err := upgradeCommand(downloadedDvmFilepath, dvmExecutablePath); err != nil {
+		return errors.Wrap(err, "upgrade fail")
 	}
 
 	ps := exec.Command(dvmExecutablePath, "--help")
