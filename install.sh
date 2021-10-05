b87f3f587c41bc637f3d0075af3496d2bde36dbc
Axetroy <axetroy.dev@gmail.com>
2021-10-05 13:41:24 +0800

    chore: update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
 set -e
 
 downloadFolder="${HOME}/Downloads"
+owner="axetroy"
+repo="dvm"
+exe_name="dvm"
 
 mkdir -p ${downloadFolder}
 
@@ -14,6 +18,9 @@ get_arch() {
     "i386" | "i486" | "i586")
         echo "386"
         ;;
+    "aarch64" | "arm64" | "arm")
+        echo "arm64"
+        ;;
     *)
         echo ${NIL}
         ;;
@@ -24,49 +31,44 @@ get_os(){
     echo $(uname -s | awk '{print tolower($0)}')
 }
 
-main() {
-    local os=$(get_os)
-    local arch=$(get_arch)
-    local dest_file="${downloadFolder}/dvm_${os}_${arch}.tar.gz"
+os=$(get_os)
+arch=$(get_arch)
+dest_file="${downloadFolder}/${exe_name}_${os}_${arch}.tar.gz"
 
-    if [[ $# -eq 0 ]]; then
-        asset_path=$(
-            command curl -sSf https://github.com/axetroy/dvm/releases |
-                command grep -o "/axetroy/dvm/releases/download/.*/dvm_${os}_${arch}\\.tar.gz" |
-                command head -n 1
-        )
-        if [[ ! "$asset_path" ]]; then exit 1; fi
-        asset_uri="https://github.com${asset_path}"
-    else
-        asset_uri="https://github.com/axetroy/dvm/releases/download/${1}/dvm_${os}_${arch}\\.tar.gz"
-    fi
+if [ $# -eq 0 ]; then
+    asset_path=$(
+        command curl -sSf https://github.com/${owner}/${repo}/releases |
+            command grep -o "/${owner}/${repo}/releases/download/.*/${exe_name}_${os}_${arch}.tar.gz" |
+            command head -n 1
+    )
+    if [[ ! "$asset_path" ]]; then exit 1; fi
+    asset_uri="https://github.com${asset_path}"
+else
+    asset_uri="https://github.com/${owner}/${repo}/releases/download/${1}/${exe_name}_${os}_${arch}.tar.gz"
+fi
 
-    mkdir -p ${downloadFolder}
+mkdir -p ${downloadFolder}
 
-    echo "[1/3] Download ${asset_uri} to ${downloadFolder}"
-    rm -f ${dest_file}
-    # wget -P "${downloadFolder}" "${asset_uri}"
-    curl --location --output "${dest_file}" "${asset_uri}"
+echo "[1/3] Download ${asset_uri} to ${downloadFolder}"
+rm -f ${dest_file}
+curl --fail --location --output "${dest_file}" "${asset_uri}"
 
-    binDir=/usr/local/bin
+binDir=/usr/local/bin
 
-    echo "[2/3] Install dvm to the ${binDir}"
-    mkdir -p ${HOME}/bin
-    tar -xz -f ${dest_file} -C ${binDir}
-    exe=${binDir}/dvm
-    chmod +x ${exe}
+echo "[2/3] Install ${exe_name} to the ${binDir}"
+mkdir -p ${HOME}/bin
+tar -xz -f ${dest_file} -C ${binDir}
+exe=${binDir}/${exe_name}
+chmod +x ${exe}
 
-    echo "[3/3] Set environment variables"
-    echo "dvm was installed successfully to ${exe}"
-    if command -v dvm --version >/dev/null; then
-        echo "Run 'dvm --help' to get started"
-    else
-        echo "Manually add the directory to your \$HOME/.bash_profile (or similar)"
-        echo "  export PATH=${HOME}/bin:\$PATH"
-        echo "Run '$exe --help' to get started"
-    fi
+echo "[3/3] Set environment variables"
+echo "${exe_name} was installed successfully to ${exe}"
+if command -v $exe_name --version >/dev/null; then
+    echo "Run '$exe_name --help' to get started"
+else
+    echo "Manually add the directory to your \$HOME/.bash_profile (or similar)"
+    echo "  export PATH=${HOME}/bin:\$PATH"
+    echo "Run '$exe --help' to get started"
+fi
 
-    exit 0
-}
-
-main
+exit 0
